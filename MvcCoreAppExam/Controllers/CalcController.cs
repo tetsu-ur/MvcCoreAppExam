e6993cb939624fbc855dfec4ba59db8ee428c32f
konno <kocha0720@gmail.com>
2023-08-21 23:00:47 +0900

    演算子での計算、「=」で計算結果を表示するまで実装済み。 「＝」の後に再度演算子を入力すると「０」になり、式が継続されないため　この後実装していく。

--- MvcCoreAppExam/Controllers/CalcController.cs
+++ MvcCoreAppExam/Controllers/CalcController.cs
@@ -47,32 +47,50 @@ namespace MvcCoreAppExam.Controllers
         /// <returns></returns>
         public IActionResult InputKey(string clickedValue)
         {
+            //// TempDataに保存されたViewModelを復元する
+            //this.ResumeViewModel();
+            //// 入力された数値を結合する
+            //this.viewModel.InputValues += clickedValue;
+            //// 表示用の値を設定する
+            //this.viewModel.DisplayValue = this.viewModel.InputValues;
+            //// ViewModelを次回の処理用にTempDataに保存
+            //TempData["CalcViewModel"] = JsonConvert.SerializeObject(viewModel);
+            //// Viewに返却　　　　　　　　　
+            //return View(ViewName, this.viewModel);
+
             // TempDataに保存されたViewModelを復元する
             this.ResumeViewModel();
-            // 入力された数値を結合する
-            this.viewModel.InputValues += clickedValue;
-            // 表示用の値を設定する
-            this.viewModel.DisplayValue = this.viewModel.InputValues;
-            // ViewModelを次回の処理用にTempDataに保存
-            TempData["CalcViewModel"] = JsonConvert.SerializeObject(viewModel);
-            // Viewに返却　　　　　　　　　
-            return View(ViewName, this.viewModel);
-
-            string laststr = this.viewModel.InputValues;
-            //InputValuesが0以上なら
-            if (laststr.Length > 0)
+                
+            string lastStr = this.viewModel.InputValues;
+            //InputValuesが0以上なら（1度でも電卓入力していたら）
+            if (lastStr.Length > 0)
             {
-                char lastcharacter = laststr[laststr.Length - 1];
+                //式の最後が記号だったら
+                char lastcharacter = lastStr[lastStr.Length - 1];
                 if (lastcharacter == '+' || lastcharacter == '-' || lastcharacter == '×' || lastcharacter == '÷')
                 {
                     //画面の値を無視して、新しく押した値を表示する
                     this.viewModel.DisplayValue = clickedValue;
+                    this.viewModel.InputValues += clickedValue;
+                } else
+                {
+                    //記号ではなかったら
+                    // 入力された数値を結合する
+                    this.viewModel.InputValues += clickedValue;
+                    // 表示用の値を設定する
+                    this.viewModel.DisplayValue += clickedValue;
                 }
-            //初めての文字なら
-            } else
+            } else //初めての文字なら
             {
-
+                // 入力された数値を結合する
+                this.viewModel.InputValues += clickedValue;
+                // 表示用の値を設定する
+                this.viewModel.DisplayValue = this.viewModel.InputValues;
             }
+            // ViewModelを次回の処理用にTempDataに保存
+            TempData["CalcViewModel"] = JsonConvert.SerializeObject(viewModel);
+            // Viewに返却　　　　　　　　　
+            return View(ViewName, this.viewModel);
         }
 
         /// <summary>
@@ -86,71 +104,56 @@ namespace MvcCoreAppExam.Controllers
         /// <returns></returns>
         public IActionResult InputOperator(string clickedValue)
         {
-            //押した記号で処理を分けていく
-            //CならInputValuesを0にする
-            if (clickedValue == "C")
+            // TempDataに保存されたViewModelを復元する
+            this.ResumeViewModel();
+            //演算子に値が入っていなかったら画面の数字をTotalValueに保存する
+            if (string.IsNullOrEmpty(this.viewModel.PreviousOperator))
             {
-                // TempDataに保存されたViewModelを復元する
-                this.ResumeViewModel();
-                this.viewModel.InputValues = "";
-            } else
+                //これまでの計算の合計値に値を格納する(Totalへの代入をInputValuesからDisplayValueに変更)
+                if (int.TryParse(this.viewModel.DisplayValue, out int result))
+                {
+                    Console.WriteLine($"変換成功: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("変換失敗: 入力が整数ではありません");      
+                }
+                this.viewModel.TotalValue = result;
+                // 入力された数値を結合する
+                this.viewModel.InputValues += clickedValue;
+                //直近で入力された演算子を記録する
+                this.viewModel.PreviousOperator = clickedValue;
+            }
+            //直近で入力された演算子に値が入っていたら計算する
+            else
             {
-                //演算子に値が入っていなかったら画面の数字をTotalValueに保存する
-                if (string.IsNullOrEmpty(this.viewModel.PreviousOperator))
+                //total int 、display string
+                var total = this.viewModel.TotalValue;
+                var display = int.Parse(this.viewModel.DisplayValue);
+                if (this.viewModel.PreviousOperator == "+")
                 {
-                    // TempDataに保存されたViewModelを復元する
-                    this.ResumeViewModel();
-                    //これまでの計算の合計値に値を格納する(Totalへの代入をInputValuesからDisplayValueに変更)
-                    if (int.TryParse(this.viewModel.DisplayValue, out int result))
-                    {
-                        Console.WriteLine($"変換成功: {result}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("変換失敗: 入力が整数ではありません");      
-                    }
-                    this.viewModel.TotalValue = result;
-                    // 入力された数値を結合する
-                    this.viewModel.InputValues += clickedValue;
-                    //直近で入力された演算子を記録する
-                    this.viewModel.PreviousOperator = clickedValue;
-                }
-                //直近で入力された演算子に値が入っていたら計算する
-                else if (!string.IsNullOrEmpty(this.viewModel.PreviousOperator))
+                    //TotalValue＋DisplayValue
+                    this.viewModel.TotalValue = total + display;
+                } else if (this.viewModel.PreviousOperator == "-")
                 {
-                    //Total int 、Display string
-                    var Total = this.viewModel.TotalValue;
-                    var Display = this.viewModel.DisplayValue;
-                    if (this.viewModel.PreviousOperator == "+")
-                    {
-                        //TotalValue＋DisplayValue
-                        this.viewModel.TotalValue = Total + int.Parse(Display);
-                        //下記記載は全部にしているが、if文でたところでしているので不要か確認する
-                        this.viewModel.DisplayValue = this.viewModel.TotalValue.ToString();
-
-                    } else if (this.viewModel.PreviousOperator == "-")
-                    {
-                        //TotalValue-DisplayValue
-                        this.viewModel.TotalValue = Total - int.Parse(Display);
-                        this.viewModel.DisplayValue = this.viewModel.TotalValue.ToString();
-                    } else if (this.viewModel.PreviousOperator == "×")
-                    {
-                        //TotalValue×DisplayValue
-                        this.viewModel.TotalValue = Total * int.Parse(Display);
-                        this.viewModel.DisplayValue = this.viewModel.TotalValue.ToString();
-                    } else if (this.viewModel.PreviousOperator == "÷")
-                    {
-                        //TotalValue÷DisplayValue
-                        this.viewModel.TotalValue = Total / int.Parse(Display);
-                        this.viewModel.DisplayValue = this.viewModel.TotalValue.ToString();
-                    }
+                    //TotalValue-DisplayValue
+                    this.viewModel.TotalValue = total - display;
+                } else if (this.viewModel.PreviousOperator == "×")
+                {
+                    //TotalValue×DisplayValue
+                    this.viewModel.TotalValue = total * display;
+                } else if (this.viewModel.PreviousOperator == "÷")
+                {
+                    //TotalValue÷DisplayValue
+                    this.viewModel.TotalValue = total / display;
                 }
+                this.viewModel.InputValues += clickedValue;
+                this.viewModel.PreviousOperator = clickedValue;
             }
             // 表示用の値を設定する(stringに変換　※記号表示しないので数値でもいいのでは？)
             this.viewModel.DisplayValue = this.viewModel.TotalValue.ToString();
             // ViewModelを次回の処理用にTempDataに保存
             TempData["CalcViewModel"] = JsonConvert.SerializeObject(viewModel);
-
             // Viewに返却　　　　　　　　　
             return View(ViewName, this.viewModel);
         }
@@ -162,7 +165,48 @@ namespace MvcCoreAppExam.Controllers
         /// <returns></returns>
         public IActionResult InputEqual(string clickedValue)
         {
-            throw new NotSupportedException("まだ実装していません。実装よろしく。");
+            // TempDataに保存されたViewModelを復元する
+            this.ResumeViewModel();
+
+            //total int 、display string
+            var total = this.viewModel.TotalValue;
+            var display = int.Parse(this.viewModel.DisplayValue);
+            if (this.viewModel.PreviousOperator == "+")
+            {
+                //TotalValue＋DisplayValue
+                this.viewModel.TotalValue = total + display;
+            }
+            else if (this.viewModel.PreviousOperator == "-")
+            {
+                //TotalValue-DisplayValue
+                this.viewModel.TotalValue = total - display;
+            }
+            else if (this.viewModel.PreviousOperator == "×")
+            {
+                //TotalValue×DisplayValue
+                this.viewModel.TotalValue = total * display;
+            }
+            else if (this.viewModel.PreviousOperator == "÷")
+            {
+                //TotalValue÷DisplayValue
+                this.viewModel.TotalValue = total / display;
+            }
+            this.viewModel.DisplayValue = this.viewModel.TotalValue.ToString();
+            return View(ViewName, this.viewModel);
+        }
+
+        /// <summary>
+        /// クリア "C" の入力を処理するアクション
+        /// </summary>
+        /// <param name="clickedValue">押下されたクリアボタンの値 "C"</param>
+        /// <returns></returns>
+        public IActionResult InputCrear(string clickedValue)
+        {
+            // TempDataに保存されたViewModelを復元する
+            this.ResumeViewModel();
+            this.viewModel.InputValues = "";
+            this.viewModel.DisplayValue = "";
+            return View(ViewName, this.viewModel);
         }
 
         /// <summary>
